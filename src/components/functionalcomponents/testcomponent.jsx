d8d7cbacb7d6762d2a3b9cd401ac83911e6a38c2
Rakshithapn <rakshithapn.21ece@kongu.edu>
2024-01-24 23:07:49 +0530

    React

---
+++ src/components/functionalcomponents/testcomponent.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function TestComp()
+{
+    return(
+        <div>
+            <p>
+                This is a test functional Component
+            </p>
+        </div>
+    )
+}
+export default TestComp;
